825c06ad68b2240325b415418c15c39ca7a712ea
Anthony Remazeilles <anthony.remazeilles@tecnalia.com>
2020-11-03 07:44:06 +0100

    add docker checking

--- test_docker_call.py
+++ test_docker_call.py
@@ -17,8 +17,67 @@ import tempfile
 import logging
 import sys
 import xml.dom.minidom
-
+# for dind management
+import re
+import docker
+import socket
 # Check https://stackoverflow.com/questions/32899/how-do-you-generate-dynamic-parameterized-unit-tests-in-python
+
+# for dind management,
+# see https://gist.github.com/dpfoose/f96d4e4b76c2e01265619d545b77987a
+
+
+def in_docker():
+    """
+    Check if running in Docker
+    :return: Whether or not this is running in Docker container
+    """
+    try:
+        with open('/proc/1/cgroup', 'rt') as cgroup_file:
+            return 'docker' in cgroup_file.read()
+    except FileNotFoundError as e:
+        return False
+
+
+def translate_path(path):
+    """
+    :param path: A string representing a path within the container
+    :return: A string representing a path on the host (or the original path if the path is not in a bound volume)
+    """
+    binds = get_binds()
+    if path in binds.keys():
+        return binds[path]
+    exps = ['(%s)/(.*)' % key for key in binds.keys()]
+    print('path: %s' % path)
+    for exp in exps:
+        result = re.search(exp, path)
+        if result:
+            print('%s/%s' % (binds[result.group(1)], result.group(2)))
+            return '%s/%s' % (binds[result.group(1)], result.group(2))
+    raise ValueError('Path %s not present in a bind mount. Volume mount will fail when running this in Docker.' % path)
+
+
+def get_current_container():
+    """
+    Will raise ValueError if there is no container with the same hostname as the environment this is running in
+    Which indicates that this is not a docker container, or that /var/run/docker.sock is not bind mounted to
+    /var/run/docker.sock on the host (i.e. this is a container which is also a docker host).
+    :return: A dictionary containing information about the container this is running in obtained using docker api
+    """
+    hostname = socket.gethostname()
+    client = docker.from_env()
+    for container in client.containers.list():
+        if container.attrs['Config']['Hostname'] == hostname:
+            return container
+    raise ValueError('Not running in Docker container')
+
+
+def get_binds():
+    """
+    :return: A dictionary with paths in the container as keys and paths on the host as values
+    """
+    container = get_current_container()
+    return {bind.split(':')[1]: bind.split(':')[0] for bind in container.attrs['HostConfig']['Binds']}
 
 class DockerCallTest(unittest.TestCase):
     """gather program tests
@@ -77,6 +136,11 @@ class DockerCallTest(unittest.TestCase):
         """test the docker component with stored input and output
 
         """
+
+        if in_docker():
+            print("Testing within a docker!")
+        else:
+            print("Testing not done in docker")
 
         for i, one_test in enumerate(self.test_detail):
 
